80851a1578fd856feec50322acd27228e38c950d
Mohamed Mejri <mejrimed08@gmail.com>
2023-02-02 19:21:41 +0100

    add performance_slices function

--- src/train/ml/model.py
+++ src/train/ml/model.py
@@ -2,6 +2,7 @@ import os
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
+from constants import CAT_FEATURES
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -81,3 +82,18 @@ def save_model(model, name="model.pkl", path="../../model/"):
     """
     full_path = os.path.join(path, name)
     pickle.dump(model, open(full_path, "wb"))
+
+def performance_slices(X, y, preds, output="slice_output.txt"):
+    df = X.copy()
+    df["target"] = y
+    df["preds"] = preds
+    
+    with open(os.path.join(os.getcwd(), output), "w") as fp:
+        for cat_feat in CAT_FEATURES:
+            fp.write(f"**** Slicing on the '{cat_feat}' feature ****\n")
+            for cat_value in df[cat_feat].unique():
+                fp.write(f"Value = {cat_value}\n")
+                slice = df[df[cat_feat]==cat_value]
+                precision, recall, fbeta = compute_model_metrics(y=slice["target"], preds=slice["preds"])
+                metrics = f"precision: {precision} \t recall: {recall} \t f1_score: {fbeta} \n"
+                fp.write(metrics)
