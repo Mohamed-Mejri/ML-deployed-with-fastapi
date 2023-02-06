65fc1453fefb6c7c54411d8a884e5cb8c6c03570
Mohamed Mejri <mejrimed08@gmail.com>
2023-02-06 12:33:01 +0100

    add test_load_models to test_model.py

--- test_model.py
+++ test_model.py
@@ -1,9 +1,9 @@
 import os
 import pytest
 from pathlib import Path
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from src.train.ml.model import save_model
+from src.train.ml.model import save_model, load_model
 
 
 
@@ -37,3 +37,15 @@ def test_save_encoder(encoder):
     
     assert path.is_file(), "ERROR: unable to save encoder"
     os.remove("./encoder.pkl")
+
+def test_load_models():
+    try:
+        encoder = load_model("src/model/encoder.pkl")
+        model = load_model("src/model/model.pkl")
+        lb = load_model("src/model/lb.pkl")
+    except:
+        print("ERROR while loading models/encoders")
+    finally:
+        assert isinstance(encoder, OneHotEncoder), "Unexpected encoder type"
+        assert isinstance(model, RandomForestClassifier), "Unexpected model type"
+        assert isinstance(lb, LabelEncoder), "Unexpected label encoder type"
