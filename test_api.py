98a5c92d90b6b2dcae60cf8c11347fefef00fb70
Mohamed Mejri <mejrimed08@gmail.com>
2023-02-07 12:41:42 +0100

    add handling to on startup events

--- test_api.py
+++ test_api.py
@@ -12,9 +12,10 @@ def test_get():
     assert response.json() == {"greetings": "Welcome to the 3rd project of the ML Devops Engineer nanodegree developed by Mohamed Mejri"}
 
 def test_post_zero_class(zero_class):
-    response = client.post("/api", json=zero_class.to_dict())
-    assert response.status_code == 200, "ERROR while testing zero_class"
-    assert response.json() == {"predictions": "['<=50K']"}, "ERROR: didn't predict zero class"
+    with client as c:
+        response = c.post("/api", json=zero_class.to_dict())
+        assert response.status_code == 200, "ERROR while testing zero_class"
+        assert response.json() == {"predictions": "['<=50K']"}, "ERROR: didn't predict zero class"
 
 def test_post_one_class(one_class):
     response = client.post("/api", json=one_class.to_dict())
